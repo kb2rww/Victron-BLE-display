b5c5b68fabf1e0575e208c443922c7963b6f0d67
Bobby <kb2rww@users.noreply.github.com>
2026-03-31 05:36:48 -0400

    Define enum values and add datasourceStop function
    Added enum values for DataSource and new function datasourceStop.

--- datasource.h
+++ datasource.h
@@ -2,20 +2,25 @@
 #include <Arduino.h>
 
 enum DataSource {
-  SOURCE_BLE,
-  SOURCE_TTL
+  SOURCE_BLE = 0,
+  SOURCE_TTL = 1
 };
 
 extern DataSource activeSource;
 
+/* Battery data shared with UI */
 struct BatteryData {
   float V;
+  float Vs;
   float I;
+  float P;
   float soc;
+  float usedAh;
+  int   ttg;
 };
 
 extern BatteryData battery;
 
 void datasourceInit();
 void datasourcePoll();
-``
+void datasourceStop();
