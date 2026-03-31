c8eb818d0199f95890aa5bb8f5a8773e0f11a334
Bobby <kb2rww@users.noreply.github.com>
2026-03-31 05:38:26 -0400

    Refactor UI touch handling and update display texts

--- ui.cpp
+++ ui.cpp
@@ -1,12 +1,12 @@
 #include "ui.h"
-#include "keypad.h"
 #include "datasource.h"
+#include "keypad.h"
 
 #include <TFT_eSPI.h>
 #include <XPT2046_Touchscreen.h>
 
-#define TFT_BL 21
-#define TOUCH_CS 33
+#define TFT_BL     21
+#define TOUCH_CS  33
 #define TOUCH_IRQ 36
 
 TFT_eSPI tft;
@@ -14,6 +14,11 @@ XPT2046_Touchscreen ts(TOUCH_CS, TOUCH_IRQ);
 
 Page currentPage = PAGE_MAIN;
 static Page lastPage = PAGE_DIAG;
+
+/* Simple hit detection */
+static bool inside(int x,int y,int rx,int ry,int rw,int rh){
+  return x>=rx && x<=rx+rw && y>=ry && y<=ry+rh;
+}
 
 void uiInit() {
   tft.init();
@@ -42,16 +47,28 @@ void uiDrawCurrentPage() {
     case PAGE_MENU:
       tft.setCursor(10, 40);
       tft.print("Menu");
+      tft.setCursor(10, 80);
+      tft.print("Source Select");
+      tft.setCursor(10, 120);
+      tft.print("Diagnostics");
       break;
 
     case PAGE_SOURCE:
       tft.setCursor(10, 40);
-      tft.print("Data Source");
+      tft.print("Select Source");
+      tft.setCursor(10, 80);
+      tft.print("BLE");
+      tft.setCursor(10, 120);
+      tft.print("TTL");
       break;
 
     case PAGE_BLE_SETUP:
       tft.setCursor(10, 40);
       tft.print("BLE Setup");
+      tft.setCursor(10, 80);
+      tft.print("Edit MAC");
+      tft.setCursor(10, 120);
+      tft.print("Edit KEY");
       break;
 
     case PAGE_KEYPAD:
@@ -60,7 +77,10 @@ void uiDrawCurrentPage() {
 
     case PAGE_DIAG:
       tft.setCursor(10, 40);
-      tft.printf("Free Heap: %u", ESP.getFreeHeap());
+      tft.printf("Heap: %u", ESP.getFreeHeap());
+      tft.setCursor(10, 80);
+      tft.printf("Source: %s",
+        activeSource == SOURCE_BLE ? "BLE" : "TTL");
       break;
   }
 }
@@ -68,9 +88,28 @@ void uiDrawCurrentPage() {
 void uiHandleTouch() {
   if (!ts.touched()) return;
 
-  currentPage = (Page)((currentPage + 1) % 6);
+  TS_Point p = ts.getPoint();
+  int x = map(p.x, 200, 3800, 0, 320);
+  int y = map(p.y, 200, 3800, 0, 240);
+
+  if (currentPage == PAGE_MAIN) {
+    if (inside(x,y,260,0,60,30)) currentPage = PAGE_MENU;
+  }
+  else if (currentPage == PAGE_MENU) {
+    if (inside(x,y,0,70,320,40)) currentPage = PAGE_SOURCE;
+    if (inside(x,y,0,110,320,40)) currentPage = PAGE_DIAG;
+  }
+  else if (currentPage == PAGE_SOURCE) {
+    if (inside(x,y,0,70,160,40)) activeSource = SOURCE_BLE;
+    if (inside(x,y,160,70,160,40)) activeSource = SOURCE_TTL;
+    currentPage = PAGE_MAIN;
+  }
+  else if (currentPage == PAGE_DIAG) {
+    currentPage = PAGE_MAIN;
+  }
+
   uiDrawCurrentPage();
-  delay(300); // debounce
+  delay(200);
 }
 
 void uiUpdate() {
